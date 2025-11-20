bbcd1eb387f747822db3e6ce20a354437eb5d5ce
juligric <178394559+juligric@users.noreply.github.com>
2025-11-20 12:18:19 -0300

    Initial commit

---
+++ Assets/luz.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class luz : MonoBehaviour
+{
+    public float intensityBase = 3f;
+    public float intensityVariation = 1.5f;
+    public float speed = 8f;
+
+    Light l;
+
+    void Start()
+    {
+        l = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        float flicker = Mathf.PerlinNoise(Time.time * speed, 0f);
+        l.intensity = intensityBase + (flicker - 0.5f) * intensityVariation;
+    }
+}
